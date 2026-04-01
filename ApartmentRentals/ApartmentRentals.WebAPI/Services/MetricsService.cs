0edb4f10ee5b07044830a6853763442f5b354610
Bychkova <nadiajiayufon@gmail.com>
2026-04-01 11:36:02 +0300

    add docker and metrics

---
+++ ApartmentRentals/ApartmentRentals.WebAPI/Services/MetricsService.cs
@@ -0,0 +1,20 @@
+﻿public class MetricsUpdateService : BackgroundService
+{
+    private readonly TimeSpan _updateInterval = TimeSpan.FromSeconds(30);
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                UptimeMetrics.Update();
+                await Task.Delay(_updateInterval, stoppingToken);
+            }
+            catch (OperationCanceledException)
+            {
+                break;
+            }
+        }
+    }
+}
